6e1481bf39d0c071c9eda3f0cef23fec14d80708
Laura Bailey <zomanom@gmail.com>
2021-06-26 07:33:34 +1000

    Added command to join voice channel

--- SpiceBot/NumberStationLogic.cs
+++ SpiceBot/NumberStationLogic.cs
@@ -1,6 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
 namespace SpiceBot
 {
     internal class NumberStationLogic : SpiceLogic
     {
+        public override Task HandleMessage(SocketMessage message)
+        {
+            if (!message.Content.Equals("!numberStation", StringComparison.InvariantCultureIgnoreCase)) return Task.CompletedTask;
+            var guildUser = (IGuildUser) message.Author;
+            return guildUser.VoiceChannel.ConnectAsync();
+        }
     }
 }
